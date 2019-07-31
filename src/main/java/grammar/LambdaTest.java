8aea63384bec84e9e29df18d106a17a71cdc2989
xiexipeng <1368010958@qq.com>
2019-07-31 22:34:41 +0800

    lambda test

---
+++ src/main/java/grammar/LambdaTest.java
@@ -0,0 +1,37 @@
+package grammar;
+
+import java.util.*;
+
+/**
+ * <p> lambda表达式语法测试 </p>
+ *
+ * @author zhousi@maihaoche.com
+ * @date 2019/7/30 下午3:24
+ * @Version V1.0
+ */
+public class LambdaTest {
+
+    public static void main(String[] args) {
+        testOptional();
+    }
+
+    private static void testOptional(){
+        Integer integer = null;
+//        System.out.println(Optional.of(integer).orElse(new Integer(2)));
+
+        System.out.println(String.format("test 1",Optional.ofNullable(integer).orElse(new Integer(2))));
+
+        Integer integer1 = new Integer(1);
+        System.out.println(String.format("test 2",Optional.ofNullable(integer1).orElse(new Integer(2))));
+
+        List<Long> list = Arrays.asList(1L);
+        System.out.println(String.format("test 3",list.stream().min(Comparator.comparing(Long::longValue)).orElse(2L)));
+
+        System.out.println(String.format("test 4",list.stream().filter(l -> l.equals(1L)).findFirst().orElse(4L)));
+
+        System.out.println(String.format("test 5",list.stream().filter(l -> l.equals(3L)).findFirst().orElse(4L)));
+
+        System.out.println(String.format("test 6",list.stream().filter(l -> l.equals(3L)).findFirst().get()));
+
+    }
+}
